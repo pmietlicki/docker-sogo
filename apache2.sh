ed31877972926dfed3e01ddb5941c63bc6b0559d
Pascal MIETLICKI <p.mietlicki@gmail.com>
2021-01-29 18:02:51 +0100

    Update apache2.sh

--- apache2.sh
+++ apache2.sh
@@ -3,7 +3,7 @@
 # Copy distribution config files to /srv as example
 mkdir -p /srv/etc
 if [ ! -f /etc/apache2/conf-available/SOGo.conf ]; then
-	curl https://raw.githubusercontent.com/inverse-inc/sogo/master/Apache/SOGo.conf --output etc/apache2/conf-available/SOGo.conf --silent
+	curl https://raw.githubusercontent.com/inverse-inc/sogo/master/Apache/SOGo.conf --output /etc/apache2/conf-available/SOGo.conf --silent
 fi
 cp /etc/apache2/conf-available/SOGo.conf /srv/etc/apache-SOGo.conf.orig
 
