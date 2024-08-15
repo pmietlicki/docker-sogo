6440de707788550d8eada2f10145e68b1c7136e4
Pascal MIETLICKI <p.mietlicki@gmail.com>
2024-08-16 09:54:41 +1100

    Update apache2.sh

--- apache2.sh
+++ apache2.sh
@@ -7,12 +7,14 @@ mkdir -p /srv/etc
 if [ ! -f /etc/apache2/conf-available/SOGo.conf ]; then
     curl -s https://raw.githubusercontent.com/inverse-inc/sogo/master/Apache/SOGo.conf -o /etc/apache2/conf-available/SOGo.conf
 fi
-
-# Back up the original SOGo configuration
 cp /etc/apache2/conf-available/SOGo.conf /srv/etc/apache-SOGo.conf.orig
 
-# Copy the administrator's version back to the Apache configuration
-cp /srv/etc/apache-SOGo.conf /etc/apache2/conf-enabled/SOGo.conf
+# Check if custom config exists and copy, else enable default config
+if [ -f /srv/etc/apache-SOGo.conf ]; then
+	cp /srv/etc/apache-SOGo.conf /etc/apache2/conf-enabled/SOGo.conf
+else
+	a2enconf SOGo.conf
+fi
 
-# Run Apache in the foreground with no detachment
-APACHE_ARGUMENTS="-DNO_DETACH" exec /usr/sbin/apache2ctl start
+# Run apache in foreground
+exec /usr/sbin/apache2ctl -D FOREGROUND
